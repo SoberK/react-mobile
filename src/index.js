c047f99cff00239ce138d6b40bfba1de2dcb7b13
kfr <1924106306@qq.com>
2019-10-28 19:36:49 +0800

    绑定redux插件

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducers from './redux/reducer';
 import rootSaga from './redux/saga/index';
 
 const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={ store }>
